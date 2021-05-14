20f28916a6dffa86abbdb7ba097806eef1ca4067
acfunwei <13921270561@163.com>
2021-05-14 09:26:26 +0800

    重构

--- quartz-module-spring-starter/src/main/java/com/quartz/mapper/FbpWsConfigMapper.java
+++
@@ -1,8 +0,0 @@
-package com.quartz.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.quartz.bean.FbpWsConfig;
-
-public interface FbpWsConfigMapper extends BaseMapper<FbpWsConfig> {
-
-}
--- quartz-module-spring-starter/src/main/java/com/quartz/mapper/FbpWsLogMapper.java
+++
@@ -1,8 +0,0 @@
-package com.quartz.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.quartz.bean.FbpWsLog;
-
-public interface FbpWsLogMapper extends BaseMapper<FbpWsLog> {
-
-}
